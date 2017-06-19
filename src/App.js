7e331dd18f8c275b523a573cd7dc6d3ba4eb515d
Mark Nuiver <marknuiver@hoppinger.com>
2017-06-20 00:33:30 +0200

    Use drupal data as state!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './build/css/App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // Setting up initial state
+    this.state = {
+      title: '',
+      date: '',
+      organizer: '',
+      location: '',
+    }
+  }
+
+  // calling the componentDidMount() method after a component is rendered for the first time
+  componentDidMount() {
+     this.serverRequest = axios.get(this.props.source).then(event =>{
+           this.setState({
+                title: event.data[2].title[0].value,
+                date: event.data[2].field_date[0].value,
+                organizer: event.data[2].field_location[0].value,
+                location: event.data[2].field_organizer[0].value
+            });
+       });
+  }
+  // calling the componentWillUnMount() method immediately before a component is unmounted from the DOM
+  componentWillUnmount() {
+       this.serverRequest.abort();
+  }
+
   render() {
+    let printdate;
+    let cdate = (new Date(this.state.date)).toDateString();
+    if (cdate != ( new Date()).toDateString() ) {
+      printdate = <p className="App-intro">{cdate}</p>
+    }
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Drupal with React Test</h2>
+           <h2>{this.state.title}</h2>
         </div>
+        {printdate}
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.organizer}
+        </p>
+        <p className="App-intro">
+          {this.state.location}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './build/css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App source="http://dev.drupalreact.com/api/events" />,
+  document.getElementById('root')
+);
 registerServiceWorker();
